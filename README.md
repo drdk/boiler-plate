b3e81493c50bcb43c0d2fc92323617b93cda3076
Jonas Swiatek <jals@dr.dk>
2014-02-19 15:23:41 +0100

    Added git stuffs

---
+++ README.md
@@ -0,0 +1,4 @@
+Standard GIT settings:
+
+    source <(curl -s https://raw.github.com/drdk/boiler-plate/master/standard-git.sh)
+
---
+++ standard-git.sh
@@ -0,0 +1,2 @@
+curl -s https://raw.github.com/drdk/boiler-plate/master/standard-git/.gitignore > .gitignore
+curl -s https://raw.github.com/drdk/boiler-plate/master/standard-git/.gitattributes > .gitattributes
