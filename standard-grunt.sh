6a0147adcc71b30e3238e7b42db1c6fe4b019769
Jonas Swiatek <jals@dr.dk>
2014-02-19 15:29:09 +0100

    Added more commands

--- standard-grunt.sh
+++ standard-grunt.sh
@@ -1,5 +1,4 @@
 curl -s https://raw.github.com/drdk/boiler-plate/master/standard-grunt/Gruntfile.coffee > Gruntfile.coffee
 curl -s https://raw.github.com/drdk/boiler-plate/master/standard-grunt/package.json > package.json
 
-npm install -g grunt-cli
 npm install
